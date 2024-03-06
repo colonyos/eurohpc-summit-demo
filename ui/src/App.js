63798cc384414ffda691d51cc94bd01f86bb859f
Johan Kristiansson <kristiansson.johan@gmail.se>
2024-03-06 21:22:26 +0100

    wip

---
+++ ui/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import DragDrop from './DragDrop';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <DragDrop />
+      </header>
+    </div>
+  );
+}
+
+export default App;
